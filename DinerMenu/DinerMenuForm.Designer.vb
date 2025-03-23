81b1489289f751643c5f01611c00ef0f1103ae68
Payden Hoskins <paydenhoskins@gmail.com>
2025-03-23 16:36:13 -0600

    Added code

--- DinerMenu/DinerMenuForm.Designer.vb
+++ DinerMenu/DinerMenuForm.Designer.vb
@@ -22,10 +22,105 @@ Partial Class DinerMenuForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SoupButton = New System.Windows.Forms.Button()
+        Me.SaladButton = New System.Windows.Forms.Button()
+        Me.FishButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.DinerNameLabel = New System.Windows.Forms.Label()
+        Me.DisplaySpecialLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'SoupButton
+        '
+        Me.SoupButton.BackColor = System.Drawing.SystemColors.Highlight
+        Me.SoupButton.Font = New System.Drawing.Font("Consolas", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SoupButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.SoupButton.Location = New System.Drawing.Point(12, 328)
+        Me.SoupButton.Name = "SoupButton"
+        Me.SoupButton.Size = New System.Drawing.Size(169, 100)
+        Me.SoupButton.TabIndex = 0
+        Me.SoupButton.Text = "Soup"
+        Me.SoupButton.UseVisualStyleBackColor = False
+        '
+        'SaladButton
+        '
+        Me.SaladButton.BackColor = System.Drawing.SystemColors.Highlight
+        Me.SaladButton.Font = New System.Drawing.Font("Consolas", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SaladButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.SaladButton.Location = New System.Drawing.Point(211, 328)
+        Me.SaladButton.Name = "SaladButton"
+        Me.SaladButton.Size = New System.Drawing.Size(169, 100)
+        Me.SaladButton.TabIndex = 1
+        Me.SaladButton.Text = "Salad"
+        Me.SaladButton.UseVisualStyleBackColor = False
+        '
+        'FishButton
+        '
+        Me.FishButton.BackColor = System.Drawing.SystemColors.Highlight
+        Me.FishButton.Font = New System.Drawing.Font("Consolas", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FishButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.FishButton.Location = New System.Drawing.Point(406, 327)
+        Me.FishButton.Name = "FishButton"
+        Me.FishButton.Size = New System.Drawing.Size(169, 100)
+        Me.FishButton.TabIndex = 2
+        Me.FishButton.Text = "Fish"
+        Me.FishButton.UseVisualStyleBackColor = False
+        '
+        'ExitButton
+        '
+        Me.ExitButton.BackColor = System.Drawing.SystemColors.Highlight
+        Me.ExitButton.Font = New System.Drawing.Font("Consolas", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ExitButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ExitButton.Location = New System.Drawing.Point(605, 329)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(169, 100)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = False
+        '
+        'DinerNameLabel
+        '
+        Me.DinerNameLabel.AutoSize = True
+        Me.DinerNameLabel.Font = New System.Drawing.Font("Edwardian Script ITC", 40.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DinerNameLabel.Location = New System.Drawing.Point(176, 9)
+        Me.DinerNameLabel.Name = "DinerNameLabel"
+        Me.DinerNameLabel.Size = New System.Drawing.Size(455, 64)
+        Me.DinerNameLabel.TabIndex = 4
+        Me.DinerNameLabel.Text = "Payden's Dinner Menu"
+        '
+        'DisplaySpecialLabel
+        '
+        Me.DisplaySpecialLabel.AutoSize = True
+        Me.DisplaySpecialLabel.Font = New System.Drawing.Font("MV Boli", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DisplaySpecialLabel.Location = New System.Drawing.Point(235, 100)
+        Me.DisplaySpecialLabel.Name = "DisplaySpecialLabel"
+        Me.DisplaySpecialLabel.Size = New System.Drawing.Size(0, 28)
+        Me.DisplaySpecialLabel.TabIndex = 5
+        Me.DisplaySpecialLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'DinerMenuForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.PaleGoldenrod
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DisplaySpecialLabel)
+        Me.Controls.Add(Me.DinerNameLabel)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.FishButton)
+        Me.Controls.Add(Me.SaladButton)
+        Me.Controls.Add(Me.SoupButton)
+        Me.Name = "DinerMenuForm"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents SoupButton As Button
+    Friend WithEvents SaladButton As Button
+    Friend WithEvents FishButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents DinerNameLabel As Label
+    Friend WithEvents DisplaySpecialLabel As Label
 End Class
--- DinerMenu/DinerMenuForm.vb
+++ DinerMenu/DinerMenuForm.vb
@@ -1,3 +1,44 @@
-﻿Public Class DinerMenuForm
+﻿'Payden Hoskins
+'RCET2265
+'Spring 2026
+'Diner Menu Program
+'https://github.com/PaydenHoskins/DinerMenu.git
 
+
+
+Public Class DinerMenuForm
+    Private Sub SaladButton_Click(sender As Object, e As EventArgs) Handles SaladButton.Click
+        'display salad item
+        DisplaySpecialLabel.Text = "~Salad of the Day~" & vbNewLine _
+            & "Buona buona insalata" & vbNewLine _
+            & vbNewLine _
+            & "Questa è un'insalata molto" & vbNewLine _
+            & "buona con formaggio e" & vbNewLine _
+            & "sottaceti, ma noi la friggiamo."
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        'Close and end
+        Me.Close()
+        End
+    End Sub
+
+    Private Sub FishButton_Click(sender As Object, e As EventArgs) Handles FishButton.Click
+        'Displays fish item
+        DisplaySpecialLabel.Text = "~Fish of the Day~" & vbNewLine _
+            & "Hamburger Di Pesce" & vbNewLine _
+           & vbNewLine _
+           & "un delizioso piatto di pesce che" & vbNewLine _
+           & "in realtà" & vbNewLine _
+           & "è un hamburger travestito."
+    End Sub
+    Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles SoupButton.Click
+        'displays soup item
+        DisplaySpecialLabel.Text = "~Soup of the Day~" & vbNewLine _
+            & "Zuppa di vongole" & vbNewLine _
+           & vbNewLine _
+           & "Vongole sotto forma di zuppa," & vbNewLine _
+           & "ottima per le giornate" & vbNewLine _
+           & "fredde o qualcosa del genere."
+    End Sub
 End Class
